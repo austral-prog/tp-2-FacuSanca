02aa5a32701acf3e8ee18373ecd523cf307b093a
FacuSanca <fsanca@mail.austral.edu.ar>
2025-04-01 16:21:37 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,13 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    a = x > y 
+    b = x < y 
+
+    a = str(a)
+    b = str(b)
+
+    print("The result of "+x+" comes first in the dictionary than "+y+" is "+b+".")
+    print("The result of "+y+" comes first in the dictionary than "+x+" is "+a+".")
+
+earth()
